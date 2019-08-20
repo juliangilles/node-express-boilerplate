99ffc6d74f4fecd55896a103072536a8dc56759e
Julian Gilles <julian.gilles@ilume.de>
2019-08-20 10:09:15 +0200

    boilerplate code v1 - add dev

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = 8080; // default port to listen
+const port = 8080;
 
-// define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("node-express-boilerplate");
 });
 
-// start the Express server
+// Start the Express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${port}`);
